2fca52f808418ec29f67c0fd5ea3aa1e27b13cd5
hamiceis <hamiceis@hotmail.com>
2022-04-09 03:02:03 -0300

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeDescription(
+  ".html",
+  "HTML é uma linguagem baseada em marcação, onde marcamos os elementos para definir quais informações a página vai exibir."
+);
+hoverChangeDescription(
+  ".css",
+  "CSS é uma linguagem de folha de estilo composta por “camadas”, criado com o propósito de estilizar as páginas HTML."
+);
+hoverChangeDescription(
+  ".js",
+  "JavaScript é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma."
+);
+
